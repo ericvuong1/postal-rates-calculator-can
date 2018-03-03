d3aad4e803af4118dfb69198995af812ab46bb8e
xd00258 <victoriquevuong@gmail.com>
2018-03-03 00:01:31 -0500

    test 1 to 5 done

--- postal_calc.py
+++ postal_calc.py
@@ -1,19 +1,20 @@
-#def postal_code_rate_calculator():
-
 import sys
+import csv
+
 
 def main(argc, argv):
 
-    if (argc) == 0:
-        buffer = "ZERO ARGUMENT!!!"
-        print(buffer)
-        return buffer
+    if (argc < 7 or argc > 7):
+        display_text = "Usage: starting_code ending_code length width height weight post_type"
+        return display_text
 
-    if (argc) < 7:
-        buffer = "Usage: ENTER"
-        print(buffer)
-        return buffer
+    with open('postal-code-generator.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        codes = []
 
+        for row in readCSV:
+            code = row[0]
+            codes.append(code)
 
-
-#
+    if argv[0] not in codes:
+        display_text = "Error: not a valid canadian starting postal code."
--- test.py
+++ test.py
@@ -1,32 +1,57 @@
-import helper
 import pytest
-import sys
 import postal_calc
 
 
-# def test_no_args(capsys):
-#     #postal_calc.
-#     out, err = capsys.readouterr()
-#     assert out == "Usage: ENTER\n"
-#
-# def test_no_args123(capsys):
-#     postal_calc.printMessage()
-#     out, err = capsys.readouterr()
-#     assert out == "Usage: Please enter a valid canadian postal code\n"
-
-# def test_get_input_less_7args(capsys):
-#     # postal_calc.getInput()
-#     out, err = capsys.readouterr()
-
-
-def test_1():
+def test_no_args():
     parameter_count = 0
     parameters = ""
     return_rate = postal_calc.main(parameter_count, parameters)
-    assert return_rate == "ZERO ARGUMENT!!!"
+    assert return_rate == "Usage: starting_code ending_code length width height weight post_type"
 
-def test_no():
-    parameter_count = 6
-    parameters = "h4k2g2 h4k2g2 3 4 5 8"
+
+def test_args_less7():
+    parameter_count = 3
+    parameters = "h4k2g2 h1t3r3 3"
     return_rate = postal_calc.main(parameter_count, parameters)
-    assert return_rate == "Usage: ENTER"
+    assert return_rate == "Usage: starting_code ending_code length width height weight post_type"
+
+
+def test_args_more7():
+    parameter_count = 8
+    parameters = "h4k2g2 h3l1o0 1 2 3 4 Xpress"
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Usage: starting_code ending_code length width height weight post_type"
+
+
+def test_invalid_startcode():
+    parameter_count = 7
+    parameters = "123456 654321 1 2 3 4 Xpress"
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: not a valid canadian starting postal code."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def test_1():
+#     parameter_count = 0
+#     parameters = ""
+#     return_rate = postal_calc.main(parameter_count, parameters)
+#     assert return_rate == "ZERO ARGUMENT!!!"
+#
+# def test_no():
+#     parameter_count = 6
+#     parameters = "h4k2g2 h4k2g2 3 4 5 8"
+#     return_rate = postal_calc.main(parameter_count, parameters)
+#     assert return_rate == "Usage: ENTER"
