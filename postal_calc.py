7e24b38a214b7984636891e492369d8312ff2053
xd00258 <victoriquevuong@gmail.com>
2018-03-03 15:14:15 -0500

    TDD complete

--- postal_calc.py
+++ postal_calc.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 
+
 def main(argc, argv):
 
     if (argc < 7 or argc > 7):
@@ -23,27 +24,91 @@ def main(argc, argv):
         display_text = "Error: not a valid canadian destination postal code."
         return display_text
 
-
     try:
         length = float(argv[2])
-
     except:
         display_text = "Error: not a valid numerical length (cm)."
         return display_text
 
+    if length < 10:
+        display_text = "Error: length must be at least 10cm."
+        return display_text
+
+    if length > 200:
+        display_text = "Error: maximum length is 200cm."
+        return display_text
+
+    try:
+        width = float(argv[3])
+    except:
+        display_text = "Error: not a valid numerical width (cm)."
+        return display_text
+
+    if width < 10:
+        display_text = "Error: width must be at least 10cm."
+        return display_text
+
+    if width > 200:
+        display_text = "Error: maximum width is 200cm."
+        return display_text
+
+    try:
+        height = float(argv[4])
+
+    except:
+        display_text = "Error: not a valid numerical height (cm)."
+        return display_text
+
+    if height < 10:
+        display_text = "Error: height must be at least 10cm."
+        return display_text
+
+    if height > 200:
+        display_text = "Error: maximum height is 200cm."
+        return display_text
+
+    try:
+        weight = float(argv[5])
+    except:
+        display_text = "Error: not a valid numerical weight (kg)."
+        return display_text
+
+    if weight < 0.1:
+        display_text = "Error: minimum weight is 0.1kg."
+        return display_text
+
+    if weight > 30:
+        display_text = "Error: maximum weight is 30kg."
+        return display_text
+
+    post_type = argv[6]
+
+    if post_type != "Regular" and post_type != "Xpress" and post_type != "Priority":
+        display_text = "Error: not a valid post type (Regular, Xpress or Priority) case sensitive."
+        return display_text
+
+    total_cost = "{0:.2f}".format(calcVolumePrice(length,width,height) + calcWeightPrice(weight) + calcPostTypePrice(post_type))
+
+    return total_cost
 
 
+def calcVolumePrice(length, width, height):
+    price = (0.01 * length * 0.01 * width * 0.01 * height) * 5
+    return price
 
 
+def calcWeightPrice(weight):
+    price = 0.5 * weight
+    return price
 
 
-
-
-
-
-
-
-
+def calcPostTypePrice(post_type):
+    if post_type == "Regular":
+        return 5.00
+    elif post_type == "Xpress":
+        return 10.00
+    elif post_type == "Priority":
+        return 15.00
 
 
 
--- test.py
+++ test.py
@@ -44,23 +44,124 @@ def test_invalid_length_format():
     assert return_rate == "Error: not a valid numerical length (cm)."
 
 
+def test_length_less10():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "-5", "2", "3", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: length must be at least 10cm."
 
 
+def test_length_more200():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "244", "2", "3", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: maximum length is 200cm."
 
 
+def test_invalid_width_format():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "abc", "3", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: not a valid numerical width (cm)."
 
 
+def test_width_less10():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "2", "3", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: width must be at least 10cm."
 
 
+def test_width_more200():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "201", "3", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: maximum width is 200cm."
 
-# def test_1():
-#     parameter_count = 0
-#     parameters = ""
-#     return_rate = postal_calc.main(parameter_count, parameters)
-#     assert return_rate == "ZERO ARGUMENT!!!"
-#
-# def test_no():
-#     parameter_count = 6
-#     parameters = "h4k2g2 h4k2g2 3 4 5 8"
-#     return_rate = postal_calc.main(parameter_count, parameters)
-#     assert return_rate == "Usage: ENTER"
+
+def test_invalid_height_format():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "abc", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: not a valid numerical height (cm)."
+
+
+def test_height_less10():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "0", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: height must be at least 10cm."
+
+
+def test_height_more200():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "400", "4", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: maximum height is 200cm."
+
+
+def test_volume_price():
+    length = 100
+    width = 100
+    height = 100
+    price = postal_calc.calcVolumePrice(length, width, height)
+    assert price == 5.00
+
+
+def test_invalid_weight_format():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "50", "abc", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: not a valid numerical weight (kg)."
+
+
+def test_weight_less_minimum():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "50", "0", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: minimum weight is 0.1kg."
+
+
+def test_weight_more_maximum():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "50", "50", "50", "31", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: maximum weight is 30kg."
+
+
+def test_weight_price():
+    weight = 3
+    price = postal_calc.calcWeightPrice(weight)
+    assert price == 1.5
+
+
+def test_invalid_post_type():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "100", "100", "100", "1", "hello"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "Error: not a valid post type (Regular, Xpress or Priority) case sensitive."
+
+
+def test_regular_type_price():
+    post_type = "Regular"
+    price = postal_calc.calcPostTypePrice(post_type)
+    assert price == 5.00
+
+
+def test_Xpress_type_price():
+    post_type = "Xpress"
+    price = postal_calc.calcPostTypePrice(post_type)
+    assert price == 10.00
+
+
+def test_priority_type_price():
+    post_type = "Priority"
+    price = postal_calc.calcPostTypePrice(post_type)
+    assert price == 15.00
+
+
+def test_total_post_rate_cost():
+    parameter_count = 7
+    parameters = ["h4k2g2", "v9g8r7", "100", "100", "100", "1", "Xpress"]
+    return_rate = postal_calc.main(parameter_count, parameters)
+    assert return_rate == "15.50"
