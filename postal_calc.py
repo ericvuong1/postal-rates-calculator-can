92a9a2b904f299c94371a5a4faaf5182c74cfcdd
xd00258 <victoriquevuong@gmail.com>
2018-03-02 21:21:26 -0500

    continue tests

---
+++ postal_calc.py
@@ -0,0 +1,6 @@
+#def postal_code_rate_calculator():
+
+import sys
+
+if (len(sys.argv) == 0):
+    print("Usage: ENTER")
--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import helper
+import pytest
+import sys
+import postal_calc
 
-def test_postal_code():
-    assert helper.get_postal_position() == 20
 
-test_postal_code()
+def test_no_args(capsys):
+    postal_calc.
+    out, err = capsys.readouterr()
+    assert out == "Usage: ENTER\n"
+
+def test_no_args123(capsys):
+    postal_calc.printMessage()
+    out, err = capsys.readouterr()
+    assert out == "Usage: Please enter a valid canadian postal code\n"
+
+# def test_get_input_less_7args(capsys):
+#     # postal_calc.getInput()
+#     out, err = capsys.readouterr()
 
